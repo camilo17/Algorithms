d0940b2b0c08d84b03ba8b6ed22d2c3bee0bb1f5
Camilo Guerrero <cami172013@gmail.com>
2017-04-20 20:48:03 -0500

    fixed some stuff and added more comments.

--- Sorting/Insertion-Sort.js
+++ Sorting/Insertion-Sort.js
@@ -1,6 +1,11 @@
 //An efficient algorithm for sorting a small number of elements.
+//This algorithms sorts the number comparing right to left.
+//It starts off with the second element in the array [1] and compares it with the
+//first one [0]
+
 
 function insertionSort(unsortedList) {
+
     var len = unsortedList.length;
 
     for (var i = 1; i < len; i++) {
@@ -8,10 +13,12 @@ function insertionSort(unsortedList) {
 
         for (var j = i - 1; j >= 0 && (unsortedList[j] > tmp); j--) {
             //Shift the number
-            unsortedList[j+ 1] = unsortedList[j];
+            unsortedList[j+ 1] = unsortedList[j];// Because J is greater than I, it is put in it takes the position of I.
         }
         // insert the copied number at the correct position in sorted part.
         unsortedList[j + 1] = tmp;
+        // j + 1 because in the loop it was decreased by one already, so leaving it as
+        // it is would replace the shift that we did.
     }
 }
 
